4d9e815dbf812cf78b55f6e463ecfc7306d1ab62
Alexey <enslaver@list.ru>
2020-10-22 02:31:49 +0300

    reformatted code

--- src/main/java/ru/geekbrains/coursework/webshop/app/domain/AService.java
+++ src/main/java/ru/geekbrains/coursework/webshop/app/domain/AService.java
@@ -11,8 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 public abstract class AService<E, R extends ARepository<E>> {
+    private final static boolean DEBUG = true;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final boolean debug = true;
     private R repository;
     private Class<E> entitiesClass;
 
@@ -25,40 +25,30 @@ public abstract class AService<E, R extends ARepository<E>> {
     }
 
     public List<E> getAll() {
-        this.getLogger().ifPresent((myLogger) -> {
-            myLogger.debug("method name {public List<E> getAll}");
-        });
+        this.getLogger().ifPresent((myLogger) -> myLogger.debug("method name {public List<E> getAll}"));
         return this.repository.findAll();
     }
 
     public Page<E> getAll(Pageable pageable) {
-        this.getLogger().ifPresent((myLogger) -> {
-            myLogger.debug("run method name {public Page<E> getAll(Pageable pageable)} where pageable is [" + pageable + "]");
-        });
+        this.getLogger().ifPresent((myLogger) -> myLogger.debug("run method name {public Page<E> getAll(Pageable pageable)} where pageable is [" + pageable + "]"));
         this.requireNotNull(pageable, "pageable cant be NULL");
         return this.repository.findAll(pageable);
     }
 
     public Optional<E> getById(Long id) {
-        this.getLogger().ifPresent((myLogger) -> {
-            myLogger.debug("run method name {public Optional<E> getById} id is [" + id + "]");
-        });
+        this.getLogger().ifPresent((myLogger) -> myLogger.debug("run method name {public Optional<E> getById} id is [" + id + "]"));
         this.requireNotNull(id, "EntityID Cant be NULL");
         return this.repository.findById(id);
     }
 
     public void save(E entity) {
-        this.getLogger().ifPresent((myLogger) -> {
-            myLogger.debug("run method name {public void add(E entity)} entity is [" + entity + "]");
-        });
+        this.getLogger().ifPresent((myLogger) -> myLogger.debug("run method name {public void add(E entity)} entity is [" + entity + "]"));
         this.requireNotNull(entity, "Entity Cant be NULL");
         this.repository.save(entity);
     }
 
     public void delete(Long id) {
-        this.getLogger().ifPresent((myLogger) -> {
-            myLogger.debug("run method name {public void del(Long id)} id is [" + id + "]");
-        });
+        this.getLogger().ifPresent((myLogger) -> myLogger.debug("run method name {public void del(Long id)} id is [" + id + "]"));
         this.requireNotNull(id, "Entity Cant be NULL");
         this.repository.deleteById(id);
     }
@@ -70,6 +60,6 @@ public abstract class AService<E, R extends ARepository<E>> {
     }
 
     protected Optional<Logger> getLogger() {
-        return this.debug ? Optional.empty() : Optional.of(this.logger);
+        return DEBUG ? Optional.empty() : Optional.of(logger);
     }
 }
