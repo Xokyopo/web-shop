042e6f764773176fa24e50f4dfe77ba80833d7fe
Alexey <enslaver@list.ru>
2020-10-30 02:44:33 +0300

    added image entities

---
+++ src/main/java/ru/geekbrains/coursework/webshop/app/dao/ImageRepository.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.coursework.webshop.app.dao;
+
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.coursework.webshop.app.domain.entities.Image;
+
+@Repository
+public interface ImageRepository extends ARepository<Image> {
+}
