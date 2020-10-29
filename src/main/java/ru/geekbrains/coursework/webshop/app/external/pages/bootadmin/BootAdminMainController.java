847dbf71e1d9c992ba5bfcbc604a91721cdf5630
Alexey <enslaver@list.ru>
2020-10-30 02:32:41 +0300

    removed from new place "external/page/bootadmin"

---
+++ src/main/java/ru/geekbrains/coursework/webshop/app/external/pages/bootadmin/BootAdminMainController.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.coursework.webshop.app.external.pages.bootadmin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bootadmin")
+public class BootAdminMainController {
+
+    @GetMapping
+    public String showMainPage() {
+        return "/bootadmin/summary";
+    }
+}
