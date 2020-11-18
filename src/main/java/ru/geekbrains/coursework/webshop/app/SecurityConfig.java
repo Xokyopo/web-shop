241a733c8f297b600383a29e057ab5421434819f
Alexey <enslaver@list.ru>
2020-11-18 22:14:18 +0300

    fixed: removed TODO comment in SecurityConfig

--- src/main/java/ru/geekbrains/coursework/webshop/app/SecurityConfig.java
+++ src/main/java/ru/geekbrains/coursework/webshop/app/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //TODO can't change the sequence
+        //can't change the sequence
         http
                 .authorizeRequests()
                 .antMatchers("/admin/**")
