58a62265e7103ad871890c0c75d475a5766af48c
Alexey <enslaver@list.ru>
2020-11-18 21:33:19 +0300

    deleted redundant interface

--- src/main/java/ru/geekbrains/coursework/webshop/app/dao/ARepository.java
+++ src/main/java/ru/geekbrains/coursework/webshop/app/dao/ARepository.java
@@ -2,8 +2,7 @@ package ru.geekbrains.coursework.webshop.app.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 @NoRepositoryBean
-public interface ARepository<E> extends JpaRepository<E, Long>, PagingAndSortingRepository<E, Long> {
+public interface ARepository<E> extends JpaRepository<E, Long> {
 }
