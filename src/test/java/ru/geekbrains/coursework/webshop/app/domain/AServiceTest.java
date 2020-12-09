83e60bb6650f5f5360491c89d86fe0474d98f26e
Alexey <enslaver@list.ru>
2020-12-10 01:27:29 +0300

    removed all redundant Entity annotation and imports

--- src/test/java/ru/geekbrains/coursework/webshop/app/domain/AServiceTest.java
+++ src/test/java/ru/geekbrains/coursework/webshop/app/domain/AServiceTest.java
@@ -9,10 +9,6 @@ import org.springframework.data.domain.Pageable;
 import ru.geekbrains.coursework.webshop.app.CallCurrentMethodException;
 import ru.geekbrains.coursework.webshop.app.dao.ARepository;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -110,10 +106,7 @@ public class AServiceTest {
     private static class TestEntityService extends AService<TestEntity, TestEntityRepository> {
     }
 
-    @Entity
     private static class TestEntity {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
         private String name;
 
