2c873aac5b6051e2a3d387a86c9e9e221e7e4cce
Alexey <enslaver@list.ru>
2020-12-10 22:23:06 +0300

    added method "getPageOf" for crating Spring Page

--- src/test/java/ru/geekbrains/coursework/webshop/app/TestUtils.java
+++ src/test/java/ru/geekbrains/coursework/webshop/app/TestUtils.java
@@ -1,7 +1,11 @@
 package ru.geekbrains.coursework.webshop.app;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.util.List;
 
 public class TestUtils {
 
@@ -9,5 +13,9 @@ public class TestUtils {
         ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
         thymeleafViewResolver.setTemplateEngine(new SpringTemplateEngine());
         return thymeleafViewResolver;
+    }
+
+    public <T> Page<T> getPageOf(T... elements) {
+        return new PageImpl<>(List.of(elements));
     }
 }
